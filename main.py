6cdd7a20dc11aeceda4667e1473be81adae44ad8
zzuwxr <zzuwxr2020@163.com>
2025-12-09 19:56:28 +0800

    rmsnorm

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-python("hello world")
-# 这是一个测试
+import torch 
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, dim:int, eps:float=1e-8):
+        super().__init__()
+        self.dim = dim
+        self.eps = eps
+        # 可以让模型学习自动更新权重
+        self.weight = torch.nn.Parameter(torch.ones(dim))
+    
+    # 计算均方根
+    def _norm(self, x):
+        
+        return torch.rsqrt(x.pow(2).mean(-1, keepdim=Ture)+ self.eps)
+
+    # 前向传播
+    def forward(self, x):
+        rms = self._norm(x)
+        return x * rms * self.weight
